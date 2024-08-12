b338f5f0f8ac216a0a5061e7804c29e3fca63456
Josh <37798644+joshuaellis@users.noreply.github.com>
2024-08-12 16:16:21 +0100

    fix: handle failed fetches gracefully

--- src/searchApiClient.ts
+++ src/searchApiClient.ts
@@ -11,16 +11,27 @@ interface FetchArgs {
   signal: AbortSignal
 }
 
-export function fetchSearchResultsFromAPI(
+export async function fetchSearchResultsFromAPI(
   searchTerm: string,
   { signal }: FetchArgs
 ): Promise<Cat[]> {
   // Note: this API seems a bit unstable and sometimes it takes a long time to respond, especially
   // for broad searches
-  return fetch(
-    `https://cat-api-bjoerge.sanity-io1.now.sh/cats?query=${searchTerm}`,
-    {
-      signal: signal,
+  try {
+    const res = await fetch(
+      `https://cat-api-bjoerge.sanity-io1.now.sh/cats?query=${searchTerm}`,
+      {
+        signal: signal,
+      }
+    )
+
+    if (!res.ok) {
+      throw new Error(`Fetch failed with status: ${res.status}`)
     }
-  ).then((res) => res.json())
+
+    return await res.json()
+  } catch (err) {
+    console.error(err)
+    return []
+  }
 }
